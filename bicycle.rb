91bb031b3a378a5d60e2a48d6d83dd116b8c2c17
Liam Considine <liam.considine@adtegrity.com>
2017-02-21 08:53:54 -0500

    Implement recumbent bike and inheritence templating

--- bicycle.rb
+++ bicycle.rb
@@ -3,15 +3,23 @@ class Bicycle
 
   def initialize(args={})
     @size       = args[:size]
-    @chain      = args[:chain]
-    @tire_size  = args[:tire_size]
+    @chain      = args[:chain]      || default_chain
+    @tire_size  = args[:tire_size]  || default_tire_size
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    raise NotImplementedError,
+      "This #{self.class} cannot respond to:"
   end
 
   def spares
     {
-      chain:      '10-speed',
-      tire_size:  '23',
-      tape_color: tape_color
+      chain:      chain,
+      tire_size:  tire_size
     }
   end
 end
@@ -25,32 +33,72 @@ class RoadBike < Bicycle
   end
 
   def spares
-    {
-    chain:      chain,
-    tire_size:  tire_size,
-    tape_color: tape_color
-    }
+    super.merge({tape_color: tape_color})
+  end
+
+  def default_tire_size
+    '23'
   end
 end
 
 class MountainBike < Bicycle
-  attr_reader :front_shocks, :rear_shocks
+  attr_reader :front_shock, :rear_shock
 
   def initialize(args)
-    @front_shocks = args[:front_shocks]
-    @rear_shocks  = args[:rear_shocks]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
     super(args)
   end
 
   def spares
     super.merge({rear_shock: rear_shock})
   end
+
+  def default_tire_size
+    '2.1'
+  end
 end
 
-bike = Bicycle.new(
-  size:         'M',
-  tape_color:   'red'
+class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def initialize(args)
+    @flag = args[:flag]
+  end
+
+  def spares
+    super.merge({flag: flag})
+  end
+
+  def default_chain
+    '9-speed'
+  end
+
+  def default_tire_size
+    '28'
+  end
+end
+
+bent = RecumbentBike.new(flag: 'tall and orange')
+
+p bent.spares
+
+road_bike = RoadBike.new(
+  size: 'M',
+  tape_color: 'red'
 )
 
-p bike.size
-p bike.spares
+p road_bike.tire_size
+p road_bike.chain
+p road_bike.inspect
+
+mountain_bike = MountainBike.new(
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
+)
+
+p mountain_bike.tire_size
+p mountain_bike.chain
+p mountain_bike.rear_shock
+p mountain_bike.inspect
