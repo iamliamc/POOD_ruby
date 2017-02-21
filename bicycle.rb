5c61bc49e083d84339833f697f4ccb521f6e4f57
Liam Considine <liam.considine@adtegrity.com>
2017-02-21 09:15:36 -0500

    Implemented post_initalize and local_spares hook methods to decouple the classes

--- bicycle.rb
+++ bicycle.rb
@@ -5,6 +5,16 @@ class Bicycle
     @size       = args[:size]
     @chain      = args[:chain]      || default_chain
     @tire_size  = args[:tire_size]  || default_tire_size
+
+    post_initialize(args)
+  end
+
+  def post_initialize(args)
+    nil
+  end
+
+  def local_spares
+    {}
   end
 
   def default_chain
@@ -20,20 +30,19 @@ class Bicycle
     {
       chain:      chain,
       tire_size:  tire_size
-    }
+    }.merge(local_spares)
   end
 end
 
 class RoadBike < Bicycle
   attr_reader :tape_color
 
-  def initialize(args)
+  def post_initialize(args)
     @tape_color = args[:tape_color]
-    super(args)
   end
 
-  def spares
-    super.merge({tape_color: tape_color})
+  def local_spares
+    {tape_color: tape_color}
   end
 
   def default_tire_size
@@ -44,14 +53,13 @@ end
 class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shock
 
-  def initialize(args)
+  def post_initialize(args)
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
-    super(args)
   end
 
-  def spares
-    super.merge({rear_shock: rear_shock})
+  def local_spares
+    {rear_shock: rear_shock}
   end
 
   def default_tire_size
@@ -62,12 +70,12 @@ end
 class RecumbentBike < Bicycle
   attr_reader :flag
 
-  def initialize(args)
+  def post_initialize(args)
     @flag = args[:flag]
   end
 
-  def spares
-    super.merge({flag: flag})
+  def local_spares
+    {flag: flag}
   end
 
   def default_chain
